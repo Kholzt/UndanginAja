041033ea915e7f89c99857e8f32327980535f0de
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-03-10 11:01:59 +0700

    Update Helpers.php

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -34,7 +34,7 @@ class Helpers
     }
     static public function getJam($tanggal)
     {
-        $jam = date('H:m', timestamp: strtotime($tanggal));
+        $jam = date('H:i', timestamp: strtotime($tanggal));
         return  $jam;
     }
 
